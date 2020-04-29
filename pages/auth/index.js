be88b8f36c63d0738363b60e2a55ed78e6f6de04
Maciej Uberna <maciej.uberna@gmail.com>
2020-04-29 18:22:19 +0200

    Lesson389 "Next.js & Components & Pages" Using components as in usual React. Lazy loading also applies to those components as other Next.js features."

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Link from 'next/link';
+import User from '../../components/User';
 
 const authIndexPage = () => (
     <div>
         <h1>The Auth Index Page</h1>
         <p> Go to <Link href="/"><a>Main</a></Link></p>
+        <User name="Maciek" age="38"/>
     </div>
 );
 
